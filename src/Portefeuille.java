40566a388071561b5b25663e5e0fc03c0f047a3a
Coignard Pierre <pcoignard18@gmail.com>
2025-04-17 15:45:34 +0200

    maj portefeuille

--- src/Portefeuille.java
+++ src/Portefeuille.java
@@ -19,10 +19,16 @@ public class Portefeuille {
    * @return Vrai si la transaction a été effectuée, faux sinon.  
    */
   public boolean transfertDevise (Portefeuille destination, double montantJetons){
-      /**
-           FONCTION À IMPLEMENTER
-	  **/
-      return false;
+      
+	   if (montantJetons <= this.montant)
+	   {
+		   
+			destination.montant+= montantJetons;
+			this.montant -= montantJetons;
+			return true;
+	   }
+	  
+       return false;
   }
 
   /**
@@ -33,9 +39,12 @@ public class Portefeuille {
    * @return true si le montant en euros est supérieur ou égal à 0 
    */
   public boolean achatDevise (double montantEuros){
-	/**
-           FONCTION À IMPLEMENTER
-	**/
+	if ( montantEuros >= 0 )
+	{
+		this.montant = montantEuros/this.monnaie.getValeurDeJeton();
+		
+		return true;
+	}
     return false;
   }
 
